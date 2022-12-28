49001a2c6041d5f7818ce0467483e64c35f64840
ERKELSOT <1256029807@qq.com>
2022-12-28 21:47:47 +0800

    chore: lambo

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import studio from "@theatre/studio";
-import extension from "@theatre/r3f/dist/extension";
-
-studio.initialize();
-studio.extend(extension);
+// import studio from "@theatre/studio";
+// import extension from "@theatre/r3f/dist/extension";
+import { Stats } from "@react-three/drei";
+import { Leva } from "leva";
+// studio.initialize();
+// studio.extend(extension);
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Suspense fallback={null}>
+      <App />
+      <Stats />
+      <Leva collapsed />
+    </Suspense>
   </React.StrictMode>
 );
